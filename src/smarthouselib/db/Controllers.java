2babe6d95a96edd89b882df8e4faa9af988041d9
Cameri <ricardo.arturo.cabral@gmail.com>
2013-06-22 18:19:39 -0400

    ~

---
+++ src/smarthouselib/db/Controllers.java
@@ -0,0 +1,68 @@
+package smarthouselib.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author cameri
+ * @since 6/22/13
+ */
+public class Controllers
+{
+  DatabaseContext db = null;
+
+  private static String sqlQueries[] = {
+    "SELECT `id`, `controller_type`, `name`, `configuration` FROM `controllers`;"
+  };
+
+  public Controllers(DatabaseContext db)
+  {
+    this.db = db;
+  }
+
+
+  public Controller getById(int id)
+  {
+    Controller controller = new Controller(this.db);
+    if (controller.load(id))
+      return controller;
+    return null;
+  }
+
+  public Controller[] getAll()
+  {
+    ArrayList<Controller> controllers = new ArrayList<Controller>();
+    Connection _conn = DatabaseContext.getInstance().getConnection();
+    PreparedStatement stmt;
+
+    try
+    {
+      stmt = _conn.prepareStatement(sqlQueries[0]);
+      ResultSet rs = stmt.executeQuery();
+
+      while (rs.next())
+      {
+        Controller controller = new Controller(this.db);
+        int i = 1;
+        controller.setId(rs.getInt(i++));
+        controller.setControllerType(rs.getString(i++));
+        controller.setName(rs.getString(i++));
+        controller.setConfiguration(rs.getString(i++));
+        controller.setChanged(false);
+        controllers.add(controller);
+      }
+
+      rs.close();
+      stmt.close();
+
+    } catch (SQLException ex)
+    {
+      ex.printStackTrace();
+    }
+
+    return (Controller[]) controllers.toArray();
+  }
+}
---
+++ src/smarthouselib/db/Devices.java
@@ -0,0 +1,70 @@
+package smarthouselib.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author cameri
+ * @since 6/22/13
+ */
+public class Devices
+{
+
+  DatabaseContext db = null;
+
+  private static String sqlQueries[] = {
+    "SELECT `id`, `device_type`, `controller_id`, `zone_id`, `name`, `address` FROM `devices`;"
+  };
+
+  public Devices(DatabaseContext db)
+  {
+    this.db = db;
+  }
+
+  public Device getById(int id)
+  {
+    Device device = new Device(this.db);
+    if (device.load(id))
+      return device;
+    return null;
+  }
+
+  public Device[] getAll()
+  {
+    ArrayList<Device> devices = new ArrayList<Device>();
+    Connection _conn = db.getConnection();
+    PreparedStatement stmt;
+
+    try
+    {
+      stmt = _conn.prepareStatement(sqlQueries[0]);
+      ResultSet rs = stmt.executeQuery();
+
+      while (rs.next())
+      {
+        Device device = new Device(this.db);
+        int i = 1;
+        device.setId(rs.getInt(i++));
+        device.setDeviceType(rs.getString(i++));
+        device.setControllerId(rs.getInt(i++));
+        device.setZoneId(rs.getInt(i++));
+        device.setName(rs.getString(i++));
+        device.setAddress(rs.getString(i++));
+        device.setChanged(false);
+        devices.add(device);
+      }
+
+      rs.close();
+      stmt.close();
+
+    } catch (SQLException ex)
+    {
+      ex.printStackTrace();
+    }
+
+    return (Device[]) devices.toArray();
+  }
+}
